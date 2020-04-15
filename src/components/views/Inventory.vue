7b6315daf39448ae215772f07fe132cfb8099044
Nooralam khan <CoreyMShafer@gmail.com>
2020-04-15 20:52:46 +0600

    Inventory.vue file added

---
+++ src/components/views/Inventory.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="row">
+        <!-- start of cart  -->
+        <div v-for="(item,index) in items" :key="index" class="card col-md-4 col-sm-6 mb-3">
+            <router-link  :to="{path:`product/${item}`}">
+                  <img v-bind:src="item.photo" class="card-img-top" alt="">
+            </router-link>
+
+            <div class="card-body">
+                <h5 class="card-title">{{item.title}}</h5>
+                <p class="card-text">$ {{item.price | replaceDoller}}</p>
+                <router-link v-bind:item="item" :to="{path:`product/${item.id}`}" class="btn btn-success float-left">
+                  view Details
+                </router-link>
+                <a @click="addToCart(item)" class="btn btn-primary float-right">
+                 + add
+                </a>
+            </div>
+        </div>
+        <!-- end of cart  -->
+    </div>
+</template>
+
+<script>
+
+
+import axios from 'axios';
+export default {
+  props:['items'],
+  methods:{
+      addToCart(item){
+        this.$emit("newItemAdded",item);
+      },
+
+  },
+
+  mounted(){
+     console.log(this.items);
+  }
+
+}
+</script>
+
+<style scoped>
+   .container{
+     padding-top:10px;
+   }
+</style>
+
