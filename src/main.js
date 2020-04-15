9a8c89fba86b477ddbfc67443a48939c1b99a717
Nooralam khan <CoreyMShafer@gmail.com>
2020-04-15 20:51:36 +0600

    main.js file has been modified

--- src/main.js
+++ src/main.js
@@ -2,16 +2,35 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import VueRouter from 'vue-router'
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-Vue.config.productionTip = false
+import { routes } from './router/index.js';
+import Vuex from 'vuex';
 
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+
+// Globally filters
+Vue.filter('replaceDoller',function(value){
+  return value.replace(/\$/,'');
+});
+
+const router = new VueRouter({
+  routes,
+  mode:'history'
+});
+
+import { storage } from "./store/index";
+
+const store = new Vuex.Store(storage);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
+  store,
   components: { App },
   template: '<App/>'
 })
